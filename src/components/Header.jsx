2fad8a3e077aa5b5214a52693264cdee48697dcd
jineshpb <89720519+jineshpb@users.noreply.github.com>
2024-10-13 16:06:46 +0530

    added og-image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,31 +3,22 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <a
-        href="https://jineshb.me"
-        target="_blank"
-        style={{ textDecoration: "none" }}
-      >
-        <p
-          style={{
-            color: "#737373",
-            fontSize: "18px",
-            fontWeight: "bold",
-            margin: "0",
-          }}
+      <div>
+        <a
+          href="https://jineshb.me"
+          target="_blank"
+          style={{ textDecoration: "none" }}
         >
-          Jineshb.me
-        </p>
-      </a>
-      <a
-        href="https://sketchfab.com/3d-models/stylized-forest-scene-may-holiday-c4baeda6e099461ab507b48245609eae"
-        target="_blank"
-        style={{ textDecoration: "none" }}
-      >
-        <p style={{ color: "#A3A3A3", fontSize: "14px", margin: "0" }}>
-          Model courtesy of @Barsh
-        </p>
-      </a>
+          <p className="text-lg text-gray-700 font-bold">Jineshb.me</p>
+        </a>
+        <a
+          href="https://sketchfab.com/3d-models/stylized-forest-scene-may-holiday-c4baeda6e099461ab507b48245609eae"
+          target="_blank"
+          style={{ textDecoration: "none" }}
+        >
+          <p className="text-sm text-gray-500">Model courtesy of @Barsh</p>
+        </a>
+      </div>
     </div>
   );
 };
